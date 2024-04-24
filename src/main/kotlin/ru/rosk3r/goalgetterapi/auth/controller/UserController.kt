c3c8d9a0dcf6339982498f5b6e6704c9e1374ac3
rosk3r <logthelog@gmail.com>
2024-04-24 21:38:31 +0600

    add users stats

--- src/main/kotlin/ru/rosk3r/goalgetterapi/auth/controller/UserController.kt
+++ src/main/kotlin/ru/rosk3r/goalgetterapi/auth/controller/UserController.kt
@@ -6,16 +6,23 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import ru.rosk3r.goalgetterapi.auth.model.response.UserStatsResponse
+import ru.rosk3r.goalgetterapi.task.service.TaskService
 
 @RestController
 @RequestMapping("/users")
 class UserController(
     private val userService: UserService,
+    private val taskService: TaskService,
 ) {
     @GetMapping
     fun getAll(): ResponseEntity<List<User>> {
-        val users = userService.getAll()
-        return ResponseEntity.ok(users)
+        return userService.getAll().let {ResponseEntity.ok(it)}
+    }
+
+    @GetMapping("/stats")
+    fun getAllStats(): ResponseEntity<List<UserStatsResponse>> {
+        return userService.getUsersStats().let { ResponseEntity.ok(it) }
     }
 
 }
