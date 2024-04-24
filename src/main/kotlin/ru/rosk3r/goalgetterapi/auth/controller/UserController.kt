1bcab6f1342400beda99c7375b98877bbe1aa71a
rosk3r <logthelog@gmail.com>
2024-04-24 21:41:08 +0600

    refactor unused implement

--- src/main/kotlin/ru/rosk3r/goalgetterapi/auth/controller/UserController.kt
+++ src/main/kotlin/ru/rosk3r/goalgetterapi/auth/controller/UserController.kt
@@ -1,19 +1,17 @@
 package ru.rosk3r.goalgetterapi.auth.controller
 
-import ru.rosk3r.goalgetterapi.auth.model.entity.User
-import ru.rosk3r.goalgetterapi.auth.service.UserService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import ru.rosk3r.goalgetterapi.auth.model.entity.User
 import ru.rosk3r.goalgetterapi.auth.model.response.UserStatsResponse
-import ru.rosk3r.goalgetterapi.task.service.TaskService
+import ru.rosk3r.goalgetterapi.auth.service.UserService
 
 @RestController
 @RequestMapping("/users")
 class UserController(
     private val userService: UserService,
-    private val taskService: TaskService,
 ) {
     @GetMapping
     fun getAll(): ResponseEntity<List<User>> {
